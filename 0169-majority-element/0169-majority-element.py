7f10804a718a10b8d5cc46ea5106ec08b279096f
sandeep96 <85406406+Singh-Sandeep-R@users.noreply.github.com>
2024-08-02 16:23:17 +0530

    Time: 181 ms (17.94%), Space: 18.1 MB (15.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        trk = {}
-        for elem in nums :
-            if elem not in trk :
-                trk[elem] = 1
+        ## moore's voting algorithm 
+        count =1
+        el = nums[0]
+        n= len(nums)
+        for i in range(1,n):
+            if count==0:
+                el = nums[i]
+            if nums[i]==el:
+                count+=1
             else :
-                trk[elem]+=1
-        for k, v in trk.items():
-            if v>(len(nums)//2):
-                return k
+                count-=1
+            if (i==n-1) & (count!=0):
+                el = el
+        count_maj = 0
+        for i in range(n):
+            if nums[i]==el:
+                count_maj+=1
+        if count_maj > n//2:
+            return el
+                
         
