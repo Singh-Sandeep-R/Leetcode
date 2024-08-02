5397ea58a2ac95f8ec7a0aefde5c2d31d5dc8baf
sandeep96 <85406406+Singh-Sandeep-R@users.noreply.github.com>
2024-08-02 16:26:34 +0530

    Time: 161 ms (91.36%), Space: 17.8 MB (90.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -11,13 +11,7 @@ class Solution:
                 count+=1
             else :
                 count-=1
-            if (i==n-1) & (count!=0):
-                el = el
-        count_maj = 0
-        for i in range(n):
-            if nums[i]==el:
-                count_maj+=1
-        if count_maj > n//2:
-            return el
+        return el
+                
                 
         
