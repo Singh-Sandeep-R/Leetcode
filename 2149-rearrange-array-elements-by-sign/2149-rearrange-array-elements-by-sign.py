2d2995b92bec76dd17771c3d78b134ea0c0813c9
sandeep96 <85406406+Singh-Sandeep-R@users.noreply.github.com>
2024-08-03 14:15:30 +0530

    Time: 1055 ms (15.76%), Space: 46.9 MB (77.28%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,29 +1,43 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos_list = []
-        neg_list = []
-        res_list = []
-        i= 0
-        while(i<len(nums)):
-            if nums[i]>0:
-                pos_list.append(nums[i])
-            else :
-                neg_list.append(nums[i])
-            i=i+1
-        # print(pos_list)
-        # print(neg_list)
-        p = 0
-        n = 0
-        for i in range(len(nums)):
-            if i%2==0:
-                res_list.append(pos_list[i-n])
-                p = p+1
-            else :
-                res_list.append(neg_list[i-p])
-                n= n+1
+        ### Brute Force
+#         pos_list = []
+#         neg_list = []
+#         res_list = []
+#         i= 0
+#         while(i<len(nums)):
+#             if nums[i]>0:
+#                 pos_list.append(nums[i])
+#             else :
+#                 neg_list.append(nums[i])
+#             i=i+1
+#         # print(pos_list)
+#         # print(neg_list)
+#         p = 0
+#         n = 0
+#         for i in range(len(nums)):
+#             if i%2==0:
+#                 res_list.append(pos_list[i-n])
+#                 p = p+1
+#             else :
+#                 res_list.append(neg_list[i-p])
+#                 n= n+1
         
 
-        return res_list
+#         return res_list
+####### Optimal Solution #########
+        p = 0
+        n = 0
+        res_list = [0]*len(nums)
+        for i in range(len(nums)):
+            if nums[i]>0:
+                res_list[2*p] = nums[i]
+                p= p+1
+            else :
+                res_list[2*n+1] = nums[i]
+                n = n+1
+
+        return(res_list)
 
         
         
