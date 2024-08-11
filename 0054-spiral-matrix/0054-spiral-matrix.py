48b0a6898b42333b539db26f09bc00ddf5c7f9db
sandeep96 <85406406+Singh-Sandeep-R@users.noreply.github.com>
2024-08-12 00:32:47 +0530

    Time: 30 ms (87.54%), Space: 16.6 MB (37.99%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,55 +1,23 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        def upper(spiral,matrix,r_s,c_s,r_e,c_e):
-            row_list = [j for j in matrix[r_s][c_s:c_e]]
-            col_list = []
-            for i in range(r_s,r_e+1):
-                if i<= r_e:
-                    col_list.append(matrix[i][c_e])
-        #     print("Upper_row",row_list)
-        #     print("Upper_col",col_list)
-            for el in row_list :
-                spiral.append(el)
-            for el in col_list :
-                spiral.append(el)
-            return spiral
-        def lower (spiral,matrix,r_s,c_s,r_e,c_e):
-            row_list = [j for j in matrix[r_e][c_s+1:c_e+1]]
-            row_list = row_list[::-1]
-            for el in row_list:
-                spiral.append(el)
-        #     print("lower_row",row_list)
-            col_list = []
-            for i in range(r_s,r_e+1):
-                if i<= r_e:
-                    col_list.append(matrix[i][c_s])
-            col_list = col_list[::-1]
-        #     print("lower_col",col_list)
-            for el in col_list:
-                spiral.append(el)
-
-            return spiral
-        r_s = 0
-        c_s = 0
-        r_e = len(matrix)-1
-        c_e = len(matrix[0])-1
+        top = left = 0
+        right = len(matrix[0])-1
+        bottom = len(matrix)-1
         spiral = []
-        
-        while((c_e>=c_s) & (r_e>=r_s)):
-            spiral = upper(spiral,matrix,r_s,c_s,r_e,c_e)
-            r_s+=1
-            r_e = r_e
-            c_e = c_e -1
-        #     print(f"after {k}th Upper time",k)
-        #     print(r_s,r_e,c_s,c_e)
-            if ((c_e<c_s) | (r_e<r_s)):
-                break
-            spiral = lower(spiral,matrix,r_s,c_s,r_e,c_e)
-            r_e = r_e-1 
-        # #     c_e = c_e-1
-            if r_s>0:
-                c_s = c_s+1
-        #     print(f"after {k}th down time",k)
-        #     print(r_s,r_e,c_s,c_e)
+        while((left<=right) & (top<=bottom)):
+            for i in range(left,right+1):
+                spiral.append(matrix[left][i])
+            top +=1
+            for i in range(top,bottom+1):
+                spiral.append(matrix[i][right])
+            right = right -1
+            if (top<= bottom):
+                for i in range(right,left-1,-1):
+                    spiral.append(matrix[bottom][i])
+                bottom = bottom -1
+            if (right>=left):
+                for i in range(bottom,top-1,-1):
+                    spiral.append(matrix[i][left])
+                left = left+1
         return spiral
         
