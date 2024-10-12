e0aca83d44ba0cc1a5cb76f22350742d99428ce7
sandeep96 <85406406+Singh-Sandeep-R@users.noreply.github.com>
2024-10-12 17:29:33 +0530

    Time: 101 ms (42.52%), Space: 18 MB (30.10%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,14 +1,53 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        count_dict = {}
-        for num in nums :
-            if num in count_dict :
-                count_dict[num]+=1
+        
+# Below code might take space of (N)
+        # count_dict = {}
+        # for num in nums :
+        #     if num in count_dict :
+        #         count_dict[num]+=1
+        #     else :
+        #         count_dict[num] = 1
+        # check = (len(nums)//3)
+        # temp = []
+        # for key,values in count_dict.items():
+        #     if values > check :
+        #         temp.append(key)
+        # return temp
+#optimize it for O(1):
+        count1= 0
+        count2 =0
+        el1 = float('-inf')
+        if len(nums)>1:
+            el2 = float('inf')
+        else :
+            return (nums)
+        check = len(nums)//3
+        
+        for num in nums:
+            if ((count1==0) and (num!=el2)):
+                count1 =1
+                el1 = num
+            elif ((count2==0) and (num!=el1)):
+                count2=1
+                el2 = num
+            elif (num==el1):
+                count1+=1
+            elif (num==el2):
+                count2+=1
             else :
-                count_dict[num] = 1
-        check = (len(nums)//3)
+                count1 =count1-1
+                count2= count2-1
         temp = []
-        for key,values in count_dict.items():
-            if values > check :
-                temp.append(key)
-        return temp
+        count1 =0
+        count2 =0
+        for num in nums:
+            if el1==num:
+                count1+=1
+            elif el2 ==num:
+                count2+=1
+        if count1>check:
+            temp.append(el1)
+        if count2>check:
+            temp.append(el2)
+        return (temp)
