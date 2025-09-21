259df7cea6b97f1bc9e06e9bce777f3d44928fa8
sandeep96 <85406406+Singh-Sandeep-R@users.noreply.github.com>
2025-09-21 18:17:56 +0530

    Time: 4 ms (89.07%), Space: 18.8 MB (80%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -2,7 +2,4 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         map_s = {char: s.count(char) for char in set(s)}
         map_s = sorted(map_s.items(), key= lambda x: x[1],reverse = True)
-        final_string = ""
-        for tuples in map_s:
-            final_string += tuples[0]*tuples[1]
-        return final_string
+        return "".join(char*count for char,count in map_s)
