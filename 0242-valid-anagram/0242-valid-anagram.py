fc7074334822968a6e4a1e52ea4c9d6597146487
sandeep96 <85406406+Singh-Sandeep-R@users.noreply.github.com>
2025-09-21 17:29:11 +0530

    Time: 16 ms (24.91%), Space: 18.5 MB (12.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        map_s = {}
-        map_t  = {}
-        count = 0
-        for i in range(len(s)):
-            if s[i] not in map_s:
-                map_s[s[i]] = 0
-            else:
-                map_s[s[i]]+=1
-            if t[i] not in map_t:
-                map_t[t[i]] = 0
-            else:
-                map_t[t[i]]+=1
-        return (map_t==map_s)
+        return (sorted(s)==sorted(t))
+
 
 
         
